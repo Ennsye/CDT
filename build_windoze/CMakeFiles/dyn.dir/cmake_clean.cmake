f7c3506ce050ccccbea62404613ab57009a47d9b
David Eade <d.a.eade2@gmail.com>
2020-06-22 11:47:20 -0400

    Add dynlib binary for Windows

---
+++ build_windoze/CMakeFiles/dyn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dyn.dir/dynlib.c.obj"
+  "libdyn.dll"
+  "libdyn.dll.a"
+  "libdyn.dll.manifest"
+  "libdyn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dyn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
